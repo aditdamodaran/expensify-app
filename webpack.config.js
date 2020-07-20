ba241c73d6637cbd6e7937a5091a848efe9d3a4e
Adit Damodaran <aditdamodaran@gmail.com>
2020-07-20 17:11:03 -0500

    updating sourcemaps and css for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: 'development',
+module.exports = (env) => {
+  const isProduction = (env === 'production')
+  const MiniCssExtract = new MiniCssExtractPlugin({
+    filename: 'styles.css',
+  })
+
+  return {
+    mode: env,
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -16,16 +23,30 @@ module.exports = {
         {
           test: /\.s?css$/,
           use: [
-            "style-loader", 
-            "css-loader", 
-            "sass-loader"
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
           ]
         }
       ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+      MiniCssExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
     }
-};
+  }
+}
