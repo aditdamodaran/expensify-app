2ddd6f6c3ec51c51b187570c152972b89085463c
Adit Damodaran <aditdamodaran@gmail.com>
2020-07-20 16:12:02 -0500

    updating app.js

--- src/app.js
+++ src/app.js
@@ -8,20 +8,12 @@ import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 import 'react-dates/initialize';
 
-import { addExpense } from './actions/expenses'
-
 const store = configureStore();
-
-// store.dispatch
-store.dispatch(addExpense({ description : 'Water Bill', amount: 4500}))
-store.dispatch(addExpense({ description : 'Gas Bill', amount: 8500, createdAt: 1000}))
-store.dispatch(addExpense({ description : 'Rent', amount: 109500}))
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 )
-
 
 ReactDOM.render(jsx, document.getElementById('app'));
