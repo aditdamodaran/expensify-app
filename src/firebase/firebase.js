b5d5cfeb2a20106a7ade4ccaf8d3f13c713b0198
Adit Damodaran <aditdamodaran@gmail.com>
2020-08-05 13:29:23 -0500

    Setup test database environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,27 +1,113 @@
 import * as firebase from 'firebase'
 
 const config= {
-    apiKey: "AIzaSyBaqRxv-85pH2hC_CvlcQijfEjOJ19LgWo",
-    authDomain: "expensify-8bf07.firebaseapp.com",
-    databaseURL: "https://expensify-8bf07.firebaseio.com",
-    projectId: "expensify-8bf07",
-    storageBucket: "expensify-8bf07.appspot.com",
-    messagingSenderId: "430631321064",
-    appId: "1:430631321064:web:307a7822e8ed17f792f85d"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
+
 
 firebase.initializeApp(config);
 
 const database = firebase.database()
 
-database.ref().set({
-  name: 'Adit Damodaran',
-  age: 21,
-  isSingle: false,
-  location: {
-    city: 'Chicago',
-    country: 'U.S'
-  }
-})
+export { firebase, database as default }
 
-database.ref('age').set(20)
+
+
+
+// child_removed (fires when one of our expenses gets deleted)
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val())
+// })
+
+// child_changed (fires when one of our expenses gets changed)
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val())
+// })
+
+// child_added (fires when a new expense is added)
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val())
+// })
+
+// const onValueChange = database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = []
+    
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       })
+//     })
+
+//     console.log(expenses)
+// }, (e) => {
+//   console.log('error fetching', e)
+// })
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = []
+    
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       })
+//     })
+
+//     console.log(expenses)
+//   })
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 92034802932
+// })
+
+// Subscribe to reading from database with a callback
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val())
+// }, (e) => {
+//   console.log('error fetching', e)
+// })
+
+// database.ref()
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val()
+//     console.log(val)
+//   })
+//   .catch((e) => {
+//     console.log('error: ', e)
+//   })
+
+// database.ref().set({
+//   name: 'Adit Damodaran',
+//   age: 21,
+//   isSingle: false,
+//   location: {
+//     city: 'Chicago',
+//     country: 'U.S'
+//   }
+// }).then(() => {
+//   console.log('Data is saved')
+// }).catch((e) => {
+//   console.log('This failed, error: ', e)
+// })
+
+// database.ref('age').set(20)
+
+// database.ref().update({
+//   name: 'Adit',
+//   age: 22
+// })
+
+// database.ref('something').remove()
